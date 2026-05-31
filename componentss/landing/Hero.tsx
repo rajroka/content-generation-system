69334e89840d429b3bec1674b9cda620cd983cba
Raj Roka <rokaraaz@gmail.com>
2026-05-31 22:31:14 +0545

    comit

--- componentss/landing/Hero.tsx
+++ componentss/landing/Hero.tsx
@@ -64,7 +64,7 @@ export function Hero() {
         {/* Right */}
         <div className="flex items-center justify-center order-first lg:order-last">
           <Image
-            src="/final-hero.png"
+            src="/newheroimage.png"
             alt="PostSathi creator"
             width={520}
             height={580}
