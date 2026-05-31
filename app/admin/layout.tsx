5bd210496f140a63af847fe091f4145986dbd2ef
Raj Roka <rokaraaz@gmail.com>
2026-05-31 22:42:56 +0545

    comit

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { auth, clerkClient } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { AdminSidebar } from "@/componentss/admin/AdminSidebar";
